f21a564e271857a44fa1d90a0155c9865121ed24
Samuel Levi Araújo Alves <samuel.levi@senior.com.br>
2021-08-04 09:15:03 -0300

    Sorting

---
+++ src/OrdenaStrings.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class OrdenaStrings {
+
+	public static void main(String[] args) {
+
+		List<String> palavras = new ArrayList<>();
+		palavras.add("Samuel");
+		palavras.add("Levi Araújo");
+		palavras.add("Alves");
+		palavras.add("Daniela");
+		palavras.add("Pereira");
+		palavras.add("Paulo Levi");
+		/**
+		 * Método antigo de ordenação de listas, utilizando um comparador criado em
+		 * classe separada.
+		 */
+		ComparadorPorTamanho comparador = new ComparadorPorTamanho();
+		Collections.sort(palavras, comparador);
+		palavras.forEach(System.out::println);
+	}
+}
+
+/**
+ * Classe de comparação que implementa a interface Comparator, tem apenas 1
+ * método.
+ * 
+ * @author samuel.levi
+ *
+ */
+class ComparadorPorTamanho implements Comparator<String> {
+	/**
+	 * Compara baseado no tamanho (length) da String.
+	 */
+	@Override
+	public int compare(String s1, String s2) {
+		if (s1.length() < s2.length()) {
+			return -1;
+		}
+		if (s1.length() > s2.length()) {
+			return 1;
+		}
+		return 0;
+	}
+}
